3cdee970ed7e9eb4d7e6b221766887deda3e0bcf
longhoanglekim <115090953+longhoanglekim@users.noreply.github.com>
2024-03-30 10:01:16 +0700

    Add testcase.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,30 +3,42 @@ package org.example;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static String passingEntranceExam(String field, double score) {
-        if (!field.equals("CN1") && !field.equals("CN12")) {
-            return "Ngành không hợp lệ";
-        }
-        if (score < 0 || score > 30) {
-            return "Điểm không hợp lệ";
-        }
-        if (score < 27)
-            return "Trượt";
-        if (field.equals("CN1")) {
-            if (score < 29.15)
-                return "Trượt";
-            return "Đỗ";
-        }
-        return "Đỗ";
-    }
 
+    //public static String specialCharacters = "!@#$%^&*()-+'=<>?/[]{}|;:,.~`";
+
+    public static boolean isValidEmail(String email) {
+        // Kiểm tra xem email có ký tự '@'
+        if (!email.contains("@")) {
+            return false;
+        }
+        int atIndex = email.lastIndexOf('@');
+        int dotBeforeAtIndex = email.indexOf('.');
+        int dotAfterAtIndex = email.lastIndexOf('.');
+
+        // Kiểm tra xem email có ít nhất một ký tự trước và sau ký tự '@'
+        if (atIndex <= 0 || atIndex >= email.length() - 1) {
+            return false;
+        }
+
+
+        // Kiểm tra xem email đủ dài từ 5 đến 320 ký tự
+        if (email.length() > 20 || email.length() < 5) {
+            return false;
+        }
+
+        // Kiểm tra xem có ít nhất một ký tự sau ký tự '.' và '.' không phải là ký tự cuối cùng
+        if (dotAfterAtIndex <= atIndex || dotAfterAtIndex >= email.length() - 1 || dotBeforeAtIndex <= atIndex) {
+            return false;
+        }
+
+        // Kiểm tra xem không có khoảng trắng
+        if (email.contains(" ")) {
+            return false;
+        }
+
+        return true;
+    }
     public static void main(String[] args) {
-        System.out.println(passingEntranceExam("CN1",10));
-        System.out.println(passingEntranceExam("CN1",27));
-        System.out.println(passingEntranceExam("CN1", 29.15));
-        System.out.println(passingEntranceExam("CN12",10));
-        System.out.println(passingEntranceExam("CN12",27));
-        System.out.println(passingEntranceExam("CN1", 29.15));
-        System.out.println(passingEntranceExam("CN8",30));
+        System.out.println(isValidEmail("a@b"));
     }
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -3,12 +3,27 @@ import org.junit.Assert;
 import org.junit.Test;
 public class MainTest {
     @Test
-    public void check_passing_entrance() {
-        Assert.assertEquals("Ngành không hợp lệ", Main.passingEntranceExam("CN8", 20));
-        Assert.assertEquals("Điểm không hợp lệ", Main.passingEntranceExam("CN1", 31));
-        Assert.assertEquals("Trượt", Main.passingEntranceExam("CN1", 10));
-        Assert.assertEquals("Trượt", Main.passingEntranceExam("CN1", 29));
-        Assert.assertEquals("Đỗ", Main.passingEntranceExam("CN1", 29.15));
-        Assert.assertEquals("Đỗ", Main.passingEntranceExam("CN12", 28));
+    public void bound_test_valid_email() {
+        Assert.assertEquals(false, Main.isValidEmail("@ab.d"));
+        Assert.assertEquals(true, Main.isValidEmail("ab@c.d"));
+        Assert.assertEquals(true, Main.isValidEmail("abcd@efg.jk"));
+        Assert.assertEquals(true, Main.isValidEmail("abcdefgh@klm.not.rty"));
+        Assert.assertEquals(true, Main.isValidEmail("abcdefg@klm.not.rty"));
+    }
+
+    @Test
+    public void tilt_table_test_valid_email() {
+        Assert.assertEquals(false, Main.isValidEmail("a@b")); //T1
+        Assert.assertEquals(false, Main.isValidEmail("@b.c")); //T2
+        Assert.assertEquals(false, Main.isValidEmail("abcde")); //T3
+        Assert.assertEquals(false, Main.isValidEmail("abcd@")); //T4
+        Assert.assertEquals(false, Main.isValidEmail("@abcd")); //T5
+        Assert.assertEquals(false, Main.isValidEmail("abcd@e")); //T6
+        Assert.assertEquals(false, Main.isValidEmail("abc.de")); //T7
+        Assert.assertEquals(false, Main.isValidEmail("abcd e")); //T8
+        Assert.assertEquals(true, Main.isValidEmail("abc@def.gh")); //T9
+        Assert.assertEquals(false, Main.isValidEmail("abc@def.gh.")); //T10
+        Assert.assertEquals(false, Main.isValidEmail(" ab c@def.gh")); //T11
+        Assert.assertEquals(false, Main.isValidEmail("abcd@efg.hjkl.mno.uvt")); //T12
     }
 }
